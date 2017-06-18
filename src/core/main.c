1cc8de412cffdf9be53a8c48bf223a86e4848303
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-06-18 16:59:09 +0000

    Put init logic in a macro, just because

--- src/core/main.c
+++ src/core/main.c
@@ -3,15 +3,16 @@
 #include "ws.h"
 #include "config.h"
 
+#define RUN_INIT(func) \
+  ret_val = func(&our_config); if(ret_val != EXIT_SUCCESS) goto exit_sleeper;
+
 int main(int argc, char *argv[], char **envp)
 {
   int ret_val;
   MAIN_CONFIG our_config;
-  
-  init_config(&our_config);
-  if(ret_val != EXIT_SUCCESS) goto exit_sleeper;
-  
-  //  init_websockets();
+
+  RUN_INIT(init_config);
+  //  RUN_INIT(init_websockets);
   
   //  world_loop();
 
